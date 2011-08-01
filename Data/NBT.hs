208da360b2555b374455a027f42750ad03304383
Adam C. Foltzer <acfoltzer@gmail.com>
2011-08-01 00:19:44 -0700

    Implemented Data.Binary.put and Arbitrary instance for NBT

--- Data/NBT.hs
+++ Data/NBT.hs
@@ -18,16 +18,17 @@ module Data.NBT where
 
 import Data.Binary ( Binary (..), decode, encode, getWord8, putWord8 )
 import Data.Binary.Get ( Get, getLazyByteString, lookAhead, skip )
+import Data.Binary.Put ( Put, putLazyByteString )
 import Data.Binary.IEEE754
 
 import qualified Codec.Compression.GZip as GZip
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.UTF8 as UTF8
-import Data.Int ( Int16, Int32, Int64 )
+import Data.Int ( Int8, Int16, Int32, Int64 )
 import Data.Word ( Word8 )
 
 import Control.Applicative ( (<$>) )
-import Control.Monad ( replicateM )
+import Control.Monad ( forM_, replicateM )
 
 import Test.QuickCheck
 
@@ -58,7 +59,7 @@ prop_TagType ty = decode (encode ty) == ty
 
 -- | Primitive representation of NBT data. 
 data NBT = EndTag
-         | ByteTag      (Maybe String) Word8
+         | ByteTag      (Maybe String) Int8
          | ShortTag     (Maybe String) Int16
          | IntTag       (Maybe String) Int32
          | LongTag      (Maybe String) Int64
@@ -133,4 +134,99 @@ instance Binary NBT where
           EndType -> skip 1 >> return []
           -- otherwise keep reading
           _ -> get >>= \tag -> (tag :) <$> getCompoundElements
-  put = undefined
+  -- | This instance handles both named and unnamed variants; a
+  -- 'Nothing' name signifies an unnamed tag, so when serialized,
+  -- neither the name nor the type tag will be put.
+  put tag = 
+    case tag of     
+      -- named cases      
+      EndTag -> put EndType
+      ByteTag (Just n) b -> 
+        put ByteType >> putName n >> putByte b
+      ShortTag (Just n) s -> 
+        put ShortType >> putName n >> putShort s
+      IntTag (Just n) i -> 
+        put IntType >> putName n >> putInt i
+      LongTag (Just n) l ->
+        put LongType >> putName n >> putLong l
+      FloatTag (Just n) f ->
+        put FloatType >> putName n >> putFloat f
+      DoubleTag (Just n) d ->
+        put DoubleType >> putName n >> putDouble d
+      ByteArrayTag (Just n) len bs ->
+        put ByteArrayType >> putName n >> putByteArray len bs
+      StringTag (Just n) len str ->
+        put StringType >> putName n >> putString str
+      ListTag (Just n) ty len ts ->
+        put ListType >> putName n >> putList ty len ts
+      CompoundTag (Just n) ts ->
+        put CompoundType >> putName n >> putCompound ts
+      -- unnamed cases
+      -- EndTag can't be unnamed
+      ByteTag Nothing b           -> putByte b
+      ShortTag Nothing s          -> putShort s
+      IntTag Nothing i            -> putInt i
+      LongTag Nothing l           -> putLong l
+      FloatTag Nothing f          -> putFloat f
+      DoubleTag Nothing d         -> putDouble d
+      ByteArrayTag Nothing len bs -> putByteArray len bs
+      StringTag Nothing len str   -> putString str
+      ListTag Nothing ty len ts   -> putList ty len ts
+      CompoundTag Nothing ts      -> putCompound ts
+    where
+      -- name and payload helpers
+      putName n           = putString n
+      putByte             = put
+      putShort            = put
+      putInt              = put
+      putLong             = put
+      putFloat            = putFloat32be
+      putDouble           = putFloat64be
+      putByteArray len bs = put len >> putLazyByteString bs
+      putString str       = let bs = UTF8.fromString str 
+                                len = (toEnum . fromEnum) (B.length bs)
+                            in put (len :: Int16) >> putLazyByteString bs
+      putList ty len ts   = put ty >> put len >> forM_ ts put
+      putCompound ts      = forM_ ts put >> put EndTag
+
+instance Arbitrary NBT where
+  arbitrary = do
+    ty <- arbitrary
+    name <- arbitrary
+    let mkArb ty name = 
+          case ty of
+            EndType -> return EndTag
+            ByteType -> ByteTag name <$> arbitrary
+            ShortType -> ShortTag name <$> arbitrary
+            IntType -> IntTag name <$> arbitrary
+            LongType -> LongTag name <$> arbitrary
+            FloatType -> FloatTag name <$> arbitrary
+            DoubleType -> DoubleTag name <$> arbitrary
+            ByteArrayType -> do
+              len <- (toEnum . fromEnum) <$> choose (0, 100 :: Int) :: Gen Int32
+              ws <- replicateM (toEnum $ fromEnum len) arbitrary
+              return $ ByteArrayTag name len $ B.pack ws
+            StringType -> do
+              n <- choose (0, 100) :: Gen Int
+              str <- replicateM (toEnum $ fromEnum n) arbitrary
+              let len' = (toEnum . fromEnum) (B.length (UTF8.fromString str))
+              return $ StringTag name len' str
+            ListType -> do
+              ty <- arbitrary `suchThat` (EndType /=)
+              len <- (toEnum . fromEnum) <$> choose (0, 10 :: Int) :: Gen Int32
+              ts <- replicateM (toEnum $ fromEnum len) (mkArb ty Nothing)
+              return $ ListTag name ty len ts
+            CompoundType -> do
+              n <- choose (0, 10)
+              ts <- replicateM n (arbitrary `suchThat` (EndTag /=) :: Gen NBT)
+              return $ CompoundTag name ts
+    mkArb ty (Just name)
+
+prop_NBTroundTrip :: NBT -> Bool
+prop_NBTroundTrip nbt = decode (encode nbt) == nbt
+
+test = do
+  file <- fmap GZip.decompress $ B.readFile "Test/testWorld/level.dat"
+  let dec = (decode file :: NBT)
+      enc = encode dec
+  return $ (enc == file, decode enc == dec)
